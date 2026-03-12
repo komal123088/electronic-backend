824151f9a4d3a053ff7b944f5e7cdc712e3cb3e4
Komal Raza <komal123088@gmail.com>
2026-03-12 21:09:59 +0500

    fix path-to-regexp wildcard for Express v5

--- server.js
+++ server.js
@@ -6,17 +6,25 @@ import productRoutes from "./routes/productRoutes.js";
 import saleRoutes from "./routes/saleRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 import customerRoutes from "./routes/customerRoutes.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-connectDB();
+// CORS setup
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
 
-app.use(cors());
+app.options("/*all", cors());
+
 app.use(express.json());
 
-// ─── Routes ───────────────────────────────────────────────────────────────────
+// Routes
 app.use("/api/products", productRoutes);
 app.use("/api/sales", saleRoutes);
 app.use("/api/customers", customerRoutes);
@@ -27,6 +35,4 @@ app.get("/", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+export default app;
