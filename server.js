b6e4042951c1cff3b69efc436c7cd04bea2f749f
Komal Raza <komal123088@gmail.com>
2026-03-16 10:44:03 +0500

    update

--- server.js
+++ server.js
@@ -4,35 +4,33 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import saleRoutes from "./routes/saleRoutes.js";
-import errorHandler from "./middleware/errorHandler.js";
 import customerRoutes from "./routes/customerRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
+import journalRoutes from "./routes/journalRoutes.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// CORS setup
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  }),
-);
+connectDB();
 
-app.options("/*all", cors());
-
+app.use(cors());
 app.use(express.json());
 
-// Routes
+// ─── Routes ───────────────────────────────────────────────────────────────────
 app.use("/api/products", productRoutes);
 app.use("/api/sales", saleRoutes);
 app.use("/api/customers", customerRoutes);
-
+app.use("/api/payments", paymentRoutes);
+app.use("/api/journal", journalRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Shop Management API is running" });
 });
 
 app.use(errorHandler);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
