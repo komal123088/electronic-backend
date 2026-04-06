a1e8246a251a117e6f1b0216957ddff0cb36bc0d
Komal Raza <komal123088@gmail.com>
2026-04-06 09:47:31 +0500

    upda

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,3 +1,4 @@
+// routes/customerRoutes.js
 import express from "express";
 import {
   getAll,
@@ -6,15 +7,26 @@ import {
   update,
   remove,
   getSaleHistory,
+  getPurchaseHistory,
+  getSuppliers,
+  getSupplierPayables,
 } from "../controllers/customerController.js";
 
 const router = express.Router();
 
+// Basic CRUD
 router.get("/", getAll);
 router.get("/:id", getOne);
 router.post("/", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
+
+// History routes
 router.get("/:id/sales", getSaleHistory);
+router.get("/:id/purchases", getPurchaseHistory);
+
+// Supplier specific routes
+router.get("/suppliers/all", getSuppliers);
+router.get("/suppliers/payables", getSupplierPayables);
 
 export default router;
